d8b8c5108cf484be7b252489f333f0b89d9e1104
vinyla <irma.rustemovic@gmail.com>
2022-03-04 17:54:49 +0100

    initial commit

---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+export default createStore(
+  composeWithDevTools(applyMiddleware(thunk))
+);
